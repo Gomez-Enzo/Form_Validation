313eaba8f9531f32fc3d51c675eb7f9c9e461b05
Gomez Enzo <gomezenzo1128@gmail.com>
2022-08-21 10:52:50 -0300

    login background

--- form_validation/lib/widgets/auth_background.dart
+++ form_validation/lib/widgets/auth_background.dart
@@ -9,6 +9,48 @@ class AuthBackground extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       color: Colors.red,
+      child: Stack(
+        children: const [
+          _PurpleBox(),
+        ],
+      ),
     );
+  }
+}
+
+class _PurpleBox extends StatelessWidget {
+  const _PurpleBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.4,
+      decoration: _PurpleBackground(),
+      child: Stack(
+        children: [],
+      ),
+    );
+  }
+
+  BoxDecoration _PurpleBackground() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        colors: [
+          Color.fromRGBO(63, 63, 156, 1),
+          Color.fromRGBO(90, 70, 178, 1),
+        ],
+      ),
+    );
+  }
+}
+
+class _buggle extends StatelessWidget {
+  const _buggle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
